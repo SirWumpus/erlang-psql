0f74a5aa7d681c8b1d8ffd2d5d85a699119cfc28
Anthony Howe <achowe@snert.com>
2021-08-23 09:57:55 -0400

    Initial working CLI.

---
+++ Makefile
@@ -0,0 +1,36 @@
+A = .a
+O = .o
+B = .beam
+E =
+.SUFFIXES : .h .c .i $O $E .hrl .erl .beam .sh
+
+PROJ		:= epsql
+
+BIN		:= _build/default/bin
+ELIB		:= _build/default/lib
+EBIN		:= ${ELIB}/${PROJ}/ebin
+ERLC_FLAGS	:= -o${EBIN}
+
+$E$B:
+	erlc ${ERLC_FLAGS} $@
+
+all: build
+
+build:
+	rebar3 escriptize
+
+clean:
+	-rm -rf src/*$B *dump
+
+distclean: clean
+	-rm -rf _build _checkouts ebin
+
+tar:
+	git archive --format tar.gz --prefix ${PROJ}/ -o ${PROJ}.tar.gz HEAD
+
+test:
+	rebar3 dialyzer
+
+# Not ready yet.
+#	rebar3 ct
+
---
+++ src/epsql.erl
@@ -0,0 +1,152 @@
+%%#!/usr/bin/env escript
+
+-module(epsql).
+-export([main/1]).
+
+-include_lib("epgsql/include/epgsql.hrl").
+
+-type io_input() :: standard_io | file:io_device().
+
+-spec usage() -> no_return().
+usage() ->
+	io:format("usage: epsql [-v][-h host][-p port][-P pass][-U user] [database]~n~n"),
+	io:format("-h host\t\thost to connect to; default \"127.0.0.1\"~n"),
+	io:format("-p port\t\tport number to connect to; default \"5432\"~n"),
+	io:format("-P pass\t\tuser password~n"),
+	io:format("-U user\t\tuser to connect as; default \"~s\"~n", [os:getenv("USER", "")]),
+	io:format("-v\t\tverbose output~n"),
+	halt(2).
+
+-spec main([string()]) -> ok.
+main(Args) ->
+	case opts:to_map(Args, [
+		{ $h, param, hostname },
+		{ $p, param, port     },
+		{ $P, param, password },
+		{ $U, param, username },
+		{ $v, flag,  verbose  }
+	]) of
+	{error, Reason, Opt} ->
+		io:format("~s -~c~n", [Reason, Opt]),
+		usage();
+	{ok, _Options, ArgsN} ->
+		process(ArgsN)
+	end.
+
+-spec process(egetopt:args() | [undefined]) -> ok.
+process([]) ->
+	process([undefined]);
+process([Database | _Args]) ->
+	PgOpts0 = #{
+		host => opts:get(hostname, "127.0.0.1"),
+		port => list_to_integer(opts:get(portnum, "5432")),
+		username => opts:get(username, os:getenv("USER"))
+	},
+	PgOpts1 = map_set(PgOpts0, password, opts:get(password)),
+	PgOpts2 = map_set(PgOpts1, database, Database),
+	case epgsql:connect(PgOpts2) of
+	{error, Reason} ->
+		io:format(standard_error, "epsql: ~p~n", [Reason]);
+	{ok, C} ->
+		psql(C, standard_io),
+		epgsql:close(C)
+	end.
+
+-spec map_set(map(), atom(), egetopt:optarg()) -> map().
+map_set(Map0, _Key, undefined) ->
+	Map0;
+map_set(Map0, Key, Value) ->
+	maps:put(Key, Value, Map0).
+
+-spec psql(epgsql:connection(), io_input()) -> ok.
+psql(C, Fp) ->
+	% Read and SQL statement.
+	case read_delim(Fp, $;) of
+	{ok, Sql} ->
+		case epgsql:equery(C, Sql) of
+		{error, Reason} ->
+			io:format(standard_error, "epsql: ~s~n", [Reason#error.message]);
+		Result ->
+			result(Result, opts:get(verbose, false)),
+			psql(C, Fp)
+		end;
+	{error, Reason} ->
+		io:format(standard_error, "epsql: ~p~n", [Reason]);
+	eof ->
+		ok
+	end.
+
+-spec read_delim(io_input(), char()) -> {ok, string()} | {error, any()} | eof.
+read_delim(Fp, Delim) ->
+	read_delim(Fp, Delim, []).
+
+-spec read_delim(io_input(), char(), [string()]) -> {ok, string()} | {error, any()} | eof.
+read_delim(Fp, Delim, Acc) ->
+	case file:read(Fp, 1) of
+	{ok, [Delim]} ->
+		{ok, lists:reverse(Acc)};
+	{ok, Byte} ->
+		read_delim(Fp, Delim, [Byte | Acc]);
+	eof ->
+		case lists:reverse(Acc) of
+		[] ->
+			eof;
+		String ->
+			{ok, String}
+		end;
+	Other ->
+		Other
+	end.
+
+-spec result(tuple(), boolean()) -> ok.
+result({ok, _Count}, false) ->
+	% INSERT / UPDATE / DELETE
+	ok;
+result({ok, Count}, true) ->
+	% INSERT / UPDATE / DELETE
+	io:format("rows ~p~n", [Count]);
+result({ok, _Cols, Rows}, _) when length(Rows) == 0 ->
+	% SELECT
+	ok;
+result({ok, Cols, Rows}, _) ->
+	% SELECT
+	headings(Cols),
+	rows(Rows);
+result({ok, _Count, Cols, Rows}, _) ->
+	% INSERT...RETURNING...
+	headings(Cols),
+	rows(Rows).
+
+-spec rows([tuple()]) -> ok.
+rows([]) ->
+	ok;
+rows([Row | Rows]) ->
+	cols(tuple_to_list(Row)),
+	rows(Rows).
+
+-spec comma_nl(non_neg_integer()) -> ok.
+comma_nl(0) ->
+	io:format("~n");
+comma_nl(_) ->
+	io:format(", ").
+
+-spec cols([any()]) -> ok.
+cols([]) ->
+	ok;
+cols([Col | Cols]) ->
+	case Col of
+	Bin when is_binary(Bin) ->
+		io:format("\"~s\"", [Bin]);
+	_ ->
+		io:format("~p", [Col])
+	end,
+	comma_nl(length(Cols)),
+	cols(Cols).
+
+-spec headings([map()]) -> ok.
+headings([]) ->
+	ok;
+headings([Col | Cols]) ->
+	io:format("\"~s\"", [Col#column.name]),
+	comma_nl(length(Cols)),
+	headings(Cols).
